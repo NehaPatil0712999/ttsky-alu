e2ef355cf0bbc374689aa35803e570c6dca7c4f0
NehaPatil0712999 <neha2023patil@gmail.com>
2026-05-09 13:16:34 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,33 +8,89 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
+    dut._log.info("Start ALU Test")
+
+    # Create Clock
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
+    # -------------------------
+    # Reset DUT
+    # -------------------------
+    dut._log.info("Reset DUT")
+
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+
+    await ClockCycles(dut.clk, 5)
+
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    # -------------------------
+    # Test 1 : Addition
+    # sel = 00
+    # A = 5
+    # B = 3
+    # Expected = 8
+    # -------------------------
+    dut._log.info("Test 1: ADDITION")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.ui_in.value = 0b00000101
+    dut.uio_in.value = 0b00000011
 
-    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 8, "Addition Failed"
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # -------------------------
+    # Test 2 : Subtraction
+    # sel = 01
+    # A = 9
+    # B = 4
+    # Expected = 5
+    # -------------------------
+    dut._log.info("Test 2: SUBTRACTION")
+
+    dut.ui_in.value = 0b01001001
+    dut.uio_in.value = 0b00000100
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 5, "Subtraction Failed"
+
+    # -------------------------
+    # Test 3 : AND
+    # sel = 10
+    # A = 12
+    # B = 10
+    # Expected = 8
+    # -------------------------
+    dut._log.info("Test 3: AND")
+
+    dut.ui_in.value = 0b10001100
+    dut.uio_in.value = 0b00001010
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 8, "AND Operation Failed"
+
+    # -------------------------
+    # Test 4 : OR
+    # sel = 11
+    # A = 12
+    # B = 10
+    # Expected = 14
+    # -------------------------
+    dut._log.info("Test 4: OR")
+
+    dut.ui_in.value = 0b11001100
+    dut.uio_in.value = 0b00001010
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 14, "OR Operation Failed"
+
+    dut._log.info("All ALU Tests Passed Successfully!")
